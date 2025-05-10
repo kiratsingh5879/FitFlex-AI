9672883764f489eb311f970fa76bafe7a5965b19
jaskirat <kiratsingh5879@gmail.com>
2025-05-10 22:12:55 +0530

    first commt

---
+++ convex/auth.config.ts
@@ -0,0 +1,9 @@
+// https://main-polliwog-15.clerk.accounts.dev
+export default {
+    providers: [
+      {
+        domain: "https://main-polliwog-15.clerk.accounts.dev",
+        applicationID: "convex",
+      },
+    ],
+  };
